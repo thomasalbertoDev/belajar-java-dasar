211bfa83754749f5f04c6395dad0a302d3ccc390
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-23 22:55:00 +0700

    learn: belajar tipe data string

---
+++ src/string/TipeDataString.java
@@ -0,0 +1,13 @@
+package string;
+
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Thomas";
+        String lastName = "Alberto";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
