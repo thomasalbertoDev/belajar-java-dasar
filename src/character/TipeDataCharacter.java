b67060523ebe5eaa31076e960cab6f33777fa634
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-23 22:43:40 +0700

    learn: belajar tipe data character

---
+++ src/character/TipeDataCharacter.java
@@ -0,0 +1,13 @@
+package character;
+
+public class TipeDataCharacter {
+    public static void main(String[] args) {
+        char m = 'm';
+        char a = 'a';
+        char s = 's';
+
+        System.out.println(m);
+        System.out.println(a);
+        System.out.println(s);
+    }
+}
