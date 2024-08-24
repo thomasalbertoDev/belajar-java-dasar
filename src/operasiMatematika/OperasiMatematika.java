77fb73c1a952b35cd17ee4a81a20344ad5fd59c4
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-24 11:38:21 +0700

    learn" Belajar operasi matematika di java

---
+++ src/operasiMatematika/OperasiMatematika.java
@@ -0,0 +1,39 @@
+package operasiMatematika;
+
+public class OperasiMatematika {
+
+    public static void main(String[] args) {
+        // TODO: Operasi Matematika
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        // TODO: Augmented Assignment (Operasi yang dilakukan pada variabel dirinya sendiri)
+        int c = 100;
+
+        c += 10; // c = c + 10
+        System.out.println(c);
+
+        c -= 10; // c = c - 10
+        System.out.println(c);
+
+        c *= 10; // c = c * 10
+        System.out.println(c);
+
+        // TODO: Unary Operator (Operasi yang dilakukan pada variabel dirinya sendiri)
+        int d = 100;
+
+        d++; // d = d + 1
+        System.out.println(d);
+
+        d--; // d = d - 1
+        System.out.println(d);
+
+         
+    }
+}
