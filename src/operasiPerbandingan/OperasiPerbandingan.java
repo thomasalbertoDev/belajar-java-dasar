b35f63bb64b1d1604ecdd86fdda776b7a7c9e1f7
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-24 11:42:11 +0700

    learn: Belajar operasi perbandingan

---
+++ src/operasiPerbandingan/OperasiPerbandingan.java
@@ -0,0 +1,20 @@
+package operasiPerbandingan;
+
+public class OperasiPerbandingan {
+    public static void main(String[] args) {
+        // TODO: Operasi Perbandingan
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a > b); // true
+        System.out.println(a < b); // false
+
+        System.out.println(a >= b); // true
+        System.out.println(a <= b); // false
+
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+
+        
+    }
+}
