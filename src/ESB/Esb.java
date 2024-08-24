a2a0a3b3c8830bb381d44f2b88fb7e3042cc129d
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-24 12:05:14 +0700

    learn: Belajar expression, statement dan block di java

---
+++ src/ESB/Esb.java
@@ -0,0 +1,17 @@
+package ESB;
+
+public class Esb {
+
+    public static void main(String[] args) {
+        // TODO: ESB (Expression, Statement, and Block)
+
+        // Expression (Sebuah Expression / ekspresi adalah kombinasi dari nilai, variabel, operator, dan fungsi yang dievaluasi menjadi suatu nilai. Misalnya, 2 + 3 atau x * 5 adalah ekspresi. Ekspresi selalu menghasilkan nilai.)
+        int x = 10;
+        int y = 5;
+        System.out.println(x + y); // 15 (Menghasilkan suatu nilai)
+
+        // Statement (Sebuah statement / pernyataan adalah instruksi atau perintah lengkap yang memberitahukan komputer untuk melakukan suatu tindakan. Contohnya, int x = 10; adalah statement yang memberikan nilai 10 ke variabel x. Statement bisa terdiri dari satu atau lebih ekspresi.)
+
+        // Block (Sebuah block / blok adalah sekumpulan dari statement. Block diawali dan diakhiri dengan kurung kurawal { }. Contohnya, { int x = 10; } adalah sebuah block yang menandakan sebuah variabel x bernilai 10. Contoh lainnya adalah public class MyClass {...ini adalah sebuah blok.})
+    }
+}
