43b805ebad397bd3ef5dc30473609fac942f138f
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-25 11:54:30 +0700

    learn: belajar for each di java

---
+++ src/foreach/ForEach.java
@@ -0,0 +1,11 @@
+package foreach;
+
+public class ForEach {
+    public static void main(String[] args) {
+        // TODO: For Each
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for(var car: cars){
+            System.out.println(car);
+        }
+    }
+}
