93e8a1d476ee99ff890dd96124c7c2d08f69ef71
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-25 11:21:29 +0700

    learn: Belajar ternary operator di java

---
+++ src/ternaryOperator/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        // TODO: Ternary Operator
+        int nilai = 70;
+        String ucapan = nilai >= 75 ? "Selamat anda lulus!" : "Maaf anda tidak lulus!";
+        System.out.println(ucapan);
+    }
+}
