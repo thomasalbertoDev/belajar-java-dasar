0a7973814faf6375fb388c623b740f973fd87247
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-25 12:03:49 +0700

    learn: belajar method atau function di java

---
+++ src/method/Method.java
@@ -0,0 +1,12 @@
+package method;
+
+public class Method {
+    public static void main(String[] args) {
+        sayHello();
+    }
+
+    static  void sayHello(){
+        System.out.println("Hello World!");
+    }
+}
+
