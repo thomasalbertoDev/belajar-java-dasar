ec73318c3e48ad704ffa1b85a79db501902b3033
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-25 11:31:39 +0700

    learn: belajar for loop di java

---
+++ src/forLooping/ForLoop.java
@@ -0,0 +1,12 @@
+package forLooping;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        // TODO: For Loop
+
+        // For(init statement; condition; update statement)
+        for (var counter = 1; counter <= 10; counter++) {
+            System.out.println("Perulangan ke-" + counter);
+        }
+    }
+}
